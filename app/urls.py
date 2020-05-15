9a7fed1d3302e420a66bbc32b1dc830ee08bc009
haruyasu <harukun2002@gmail.com>
2020-05-15 09:41:09 +0900

    :rocket: 順番修正

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,9 @@ from app import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('order/', views.OrderView.as_view(), name='order'),
     path('product/<slug>', views.ItemDetailView.as_view(), name='product'),
     path('additem/<slug>', views.addItem, name='additem'),
+    path('order/', views.OrderView.as_view(), name='order'),
     path('removeitem/<slug>', views.removeItem, name='removeitem'),
     path('removesingleitem/<slug>', views.removeSingleItem, name='removesingleitem'),
     path('payment/', views.PaymentView.as_view(), name='payment'),
